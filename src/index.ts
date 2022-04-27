6b0ab8d239c08f5ef4cf0249d304186bacd13112
sandra248 <104040301+sandra248@users.noreply.github.com>
2022-04-26 21:26:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let precioInicial: number = 450.5;
+let descuento: number = 0.1;
+let precioFinal: number = precioInicial - (precioInicial * descuento);
+console.log("El precio Final es", precioFinal);
